0b9814c0a30a34f5e1f255d115b40af9a80af42e
Erick <201616201c@utea.edu.pe>
2022-10-12 20:47:10 -0500

    inicio sesion

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>todolist</title>
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+
+        <h1>TodoList</h1><br>
+        <h3>inicie sesion</h3> 
+        o <a href="registro.php">registrese</a>
+        <form action="index.php" method="post">
+            <input type="text" name="email"  placeholder="ingrese su correo">
+            <input type="password" name="contraseña" placeholder="ingrese su contraseña">
+            <input type="submit" value="ingresar">
+        </form>
+    
+    
+    
+</body>
+</html>
